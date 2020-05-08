395b18e80f607ff3632753aae5431156d21a3fec
Alexander A. Klimov <grandmaster@al2klimov.de>
2020-05-08 15:49:26 +0200

    CONNECT: offload TLS

--- proxy.go
+++ proxy.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
 	irisCtx "github.com/kataras/iris/context"
 	log "github.com/sirupsen/logrus"
+	"math/big"
 	"net"
 )
+
+var tlsOffload = func() tls.Config {
+	priv, errGK := rsa.GenerateKey(rand.Reader, 1024)
+	if errGK != nil {
+		panic(errGK)
+	}
+
+	template := x509.Certificate{SerialNumber: big.NewInt(0)}
+	cert, errCC := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
+
+	if errCC != nil {
+		panic(errCC)
+	}
+
+	return tls.Config{Certificates: []tls.Certificate{{
+		Certificate: [][]byte{cert},
+		PrivateKey:  priv,
+	}}}
+}()
 
 func proxy(ctx irisCtx.Context) {
 	_, port, errSA := net.SplitHostPort(ctx.Request().RequestURI)
@@ -15,8 +39,12 @@ func proxy(ctx irisCtx.Context) {
 		return
 	}
 
+	wrapTls := false
+
 	switch port {
-	case "80", "443":
+	case "80":
+	case "443":
+		wrapTls = true
 	default:
 		ctx.StatusCode(403)
 		return
@@ -34,4 +62,14 @@ func proxy(ctx irisCtx.Context) {
 	}
 
 	defer conn.Close()
+
+	if wrapTls {
+		tlsConn := tls.Server(conn, &tlsOffload)
+		conn = tlsConn
+
+		if errHs := tlsConn.Handshake(); errHs != nil {
+			log.WithFields(log.Fields{"error": errHs.Error()}).Error("TLS handshake failed")
+			return
+		}
+	}
 }
